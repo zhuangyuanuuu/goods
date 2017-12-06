28db4d58b2ea8b33578027c4bc735879296856ba
zhuangyuan <379893806@qq.com>
2017-12-06 17:26:04 +0800

    add GoodsApplication

---
+++ src/main/java/com/zhuangyuan/goods/GoodsApplication.java
@@ -0,0 +1,12 @@
+package com.zhuangyuan.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsApplication.class, args);
+	}
+}
