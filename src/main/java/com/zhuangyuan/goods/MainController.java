e57ef261505c585e1fc4b60daf0f9c4a71cc5fc6
zhuangyuan <379893806@qq.com>
2017-12-07 15:17:22 +0800

    add static,templates

--- src/main/java/com/zhuangyuan/goods/MainController.java
+++ src/main/java/com/zhuangyuan/goods/MainController.java
@@ -15,7 +15,7 @@ public class MainController {
 	private UserRepository userRepository;
 
 	@GetMapping(path="/add") // Map ONLY GET Requests
-	public @ResponseBody String addNewUser (@RequestParam String name
+	public String addNewUser (@RequestParam String name
 			, @RequestParam String email) {
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
