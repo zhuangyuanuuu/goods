1fc75f2b6a4a6ceb5ef6704c3ed418b1425e016c
zhuangyuan <379893806@qq.com>
2017-12-07 14:43:40 +0800

    add static,templates

---
+++ src/main/java/com/zhuangyuan/goods/MainController.java
@@ -0,0 +1,43 @@
+package com.zhuangyuan.goods;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/demo")
+public class MainController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping(path="/add") // Map ONLY GET Requests
+	public @ResponseBody String addNewUser (@RequestParam String name
+			, @RequestParam String email) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+
+		User n = new User();
+		n.setName(name);
+		n.setEmail(email);
+		userRepository.save(n);
+		return "Saved";
+	}
+
+	@GetMapping(path="/all")
+	public @ResponseBody Iterable<User> getAllUsers() {
+		// This returns a JSON or XML with the users
+		return userRepository.findAll();
+	}
+
+	public UserRepository getUserRepository() {
+		return userRepository;
+	}
+
+	public void setUserRepository(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+}
--- src/main/java/com/zhuangyuan/goods/Test.java
+++
@@ -1,4 +0,0 @@
-package com.zhuangyuan.goods;
-
-public class Test {
-}
